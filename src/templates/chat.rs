888166fae9a81d406e9cfebe31bcb54ac310af60
xTeKc <bcd3v@pm.me>
2022-02-04 19:44:35 -0500

    chat rs

--- src/templates/chat.rs
+++ src/templates/chat.rs
@@ -1,21 +1,21 @@
 use perseus::Template;
 use sycamore::prelude::{component, view, Html, SsrNode, View};
 
-#[perseus::template(AboutPage)]
-#[component(AboutPage<G>)]
-pub fn about_page() -> View<G> {
+#[perseus::template(ChatPage)]
+#[component(ChatPage<G>)]
+pub fn chat_page() -> View<G> {
     view! {
-        p { "About." }
+        p { "Chat." }
     }
 }
 
 #[perseus::head]
 pub fn head() -> View<SsrNode> {
     view! {
-        title { "About Page | Basic" }
+        title { "Chat Page | Basic" }
     }
 }
 
 pub fn get_template<G: Html>() -> Template<G> {
-    Template::new("about").template(about_page).head(head)
+    Template::new("chat").template(chat_page).head(head)
 }
